af44722c1e01b1980580b37918798825ffc91e85
anni02th <aniketmore2000th@gmail.com>
2024-07-27 15:14:51 +0530

    2nd

---
+++ client/src/components/header.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import '../main.css'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between p-4 '>
+      <div className='text-white flex gap-4'>
+         <a href="#Home">Home</a>
+         <a href="#Aboutus">About Us</a>
+      </div>
+      <div>
+         <a href="#">
+            <h1 className='text-2xl font-bold'>Univerz University</h1>
+         </a>
+      </div>
+      <div className='text-black flex gap-4'>
+         <select name="login">
+            <option value="none">Sign In</option>
+            <option value="as-Admin">as Admin</option>
+            <option value="as-Faculty">as Faculty</option>
+            <option value="as-Student">as Student</option>
+            <option value="as-Alumni">as Alumni</option>
+         </select>
+         <select name="signin" id="">
+            <option value="none">Login In</option>
+            <option value="as-Admin">as Admin</option>
+            <option value="as-Faculty">as Faculty</option>
+            <option value="as-Student">as Student</option>
+            <option value="as-Alumni">as Alumni</option>
+         </select>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/components/*{jsx,js,html,tsx}"
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
