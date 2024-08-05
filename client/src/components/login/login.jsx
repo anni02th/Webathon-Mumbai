2ef36c12b63547e06a220ca3803e38eac8a1267e
anni02th <aniketmore2000th@gmail.com>
2024-08-06 00:33:31 +0530

    updates in file

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [role, setRole] = useState('Admin');
-  const navigate = useNavigate();
+  const navigate = useNavigate(); 
   const { setUser } = useAuth();
 
   // Remove the isLoggedIn state and related effect
